a8c958e54b114c8355a7c656738a90db8697ef0b
Gabriel Gomes <gabrigomez15@gmail.com>
2021-06-11 16:34:02 -0300

    start tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: ['Sen']
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
