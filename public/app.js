87cc4301d801700b96f555954d436438ef3de0bd
Gabriel Gomes <gabrigomez15@gmail.com>
2021-06-14 16:26:13 -0300

    init app file

---
+++ public/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var form = document.getElementById('forms');
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- src/sandbox.ts
+++
@@ -1,5 +0,0 @@
-const inputs = document.querySelectorAll('input')
-
-inputs.forEach(input => {
-    console.log(input)
-})
